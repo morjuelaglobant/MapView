cd4cd34cfe6312c9cdb51197eb5b852deda58616
Miguel Orjuela <miguel.orjuela@globant.com>
2017-02-13 13:07:53 -0500

    Added source files

---
+++ MapView/MapView.h
@@ -0,0 +1,19 @@
+//
+//  MapView.h
+//  MapView
+//
+//  Created by Miguel Orjuela on 13/02/17.
+//  Copyright © 2017 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapView.
+FOUNDATION_EXPORT double MapViewVersionNumber;
+
+//! Project version string for MapView.
+FOUNDATION_EXPORT const unsigned char MapViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapView/PublicHeader.h>
+
+
